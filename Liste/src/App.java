a1ff63303eb0578d2681c224d69c2fbb2686a60a
Amin Itrbi <152690464+Amal4267@users.noreply.github.com>
2026-01-06 17:18:36 +0100

    Update App.java

--- Liste/src/App.java
+++ Liste/src/App.java
@@ -1,10 +1,22 @@
-public class App {
-   public static void main(String[] args) {
-		Lista l = new Lista();
-		l.add(new Nodo("Mario"));
-		l.add(new Nodo("Giulio"));
-		l.add(new Nodo("Francesco"));
-		l.addHead(new Nodo("Giuseppe"));
-		System.out.println(l);
-	}
+public class Main {
+    public static void main(String[] args) {
+
+        Lista l = new Lista();
+
+        l.addHead(new Nodo("A"));
+        l.add(new Nodo("B"));
+        l.add(new Nodo("C"));
+        l.addHead(new Nodo("D"));
+
+        System.out.println(l.toString());
+        System.out.println(l.toStringReverse());
+
+        System.out.println("Esiste A? " + l.exists("A"));
+        l.remove("A");
+        System.out.println("Questa è la lista dopo la rimozione di A");
+        System.out.println(l.toString());
+        
+
+    }
 }
+
